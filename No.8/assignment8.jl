e23cd0396c941f7ef668ac66465a8c994a21372c
Jan Ullmann <102742052+ullmannJan@users.noreply.github.com>
2022-12-14 10:41:52 +0100

    preparations for next assignments

---
+++ No.8/assignment8.jl
@@ -0,0 +1,40 @@
+### A Pluto.jl notebook ###
+# v0.19.15
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 38f6a9ba-7b93-11ed-1965-8fb3c2c8c466
+md"""
+# Assignment 8
+"""
+
+# ╔═╡ c14572ad-b65d-4495-ab99-1436e98f7867
+md"""
+## 7.I
+## 7.II
+## 7.III
+## 7.IV
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.2"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═38f6a9ba-7b93-11ed-1965-8fb3c2c8c466
+# ╠═c14572ad-b65d-4495-ab99-1436e98f7867
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
